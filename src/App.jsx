d88084eb228a0c14708b830d7403a5373e423dc0
BoevMaksim <maksim.schetina@gmail.com>
2021-03-12 00:50:50 +0300

    implement PizzaItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -6,11 +6,19 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 const App = () => {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((res) => res.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home pizzas={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
---
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+const PizzaItem = ({ data }) => {
+  const [pizzaSize, setActiveSize] = useState(0);
+  const [doughType, setDoughType] = useState(0);
+  const type = ['тонкое', 'традиционное'];
+
+  return (
+    <div className="pizza-block">
+      <img className="pizza-block__image" src={data.imageUrl} alt="Pizza" />
+      <h4 className="pizza-block__title">{data.name}</h4>
+      <div className="pizza-block__selector">
+        <ul>
+          {type.map((itemType, index) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+              <li
+                key={itemType}
+                onClick={() => {
+                  setDoughType(index);
+                }}
+                className={
+                  (data.types.length > 1 && doughType === index ? 'active' : '') +
+                  (data.types.length === 1 && index === data.types[0] ? 'active' : '') +
+                  (!data.types.includes(index) ? 'disabled' : '')
+                }
+              >
+                {itemType}
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          {!!data.sizes &&
+            data.sizes.map((size, index) => {
+              return (
+                // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+                <li
+                  key={index.toString()}
+                  onClick={() => {
+                    setActiveSize(index);
+                  }}
+                  className={pizzaSize === index ? 'active' : ''}
+                >
+                  {size} см.
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+      <div className="pizza-block__bottom">
+        <div className="pizza-block__price">от {data.price} ₽</div>
+        <div className="button button--outline button--add">
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+              fill="white"
+            />
+          </svg>
+          <span>Добавить</span>
+          <i>2</i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PizzaItem;
