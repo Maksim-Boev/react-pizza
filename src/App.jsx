1d295b2081d3b889ec6966d3731385b1a2caea0f
BoevMaksim <maksim.schetina@gmail.com>
2021-03-13 21:46:33 +0300

    add axios/ refactoring get pizzas data function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -9,9 +10,9 @@ const App = () => {
   const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((res) => res.json())
-      .then((json) => setPizzas(json.pizzas));
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
   }, []);
 
   return (
